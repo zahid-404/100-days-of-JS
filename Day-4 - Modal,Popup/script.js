a2af7c9bae0fc462b0f67a3425a969b9a1b9aeb0
Zahid <zahidmohammad495@gmai.com>
2023-08-24 19:48:09 +0530

    Complete Project 4

---
+++ Day-4 - Modal,Popup/script.js
@@ -0,0 +1,10 @@
+// varibales
+let modal = document.querySelector("#modal");
+
+function openModal() {
+  modal.classList.remove("translate-y-full");
+}
+
+function closeModal() {
+  modal.classList.add("translate-y-full");
+}
